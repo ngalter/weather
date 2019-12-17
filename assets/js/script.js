666487faaae24ae2b52d0f722901c3d761c25334
Nancy Alter <nancy.alter@gmail.com>
2019-12-16 22:17:59 -0500

    update colors
    update colors

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $.getJSON("./assets/json/city.list.json", function (info) {
-        for (var i = 0; i < info.length; i++) {
-            console.log(info.name[i]);
-        }
-    });
-});
